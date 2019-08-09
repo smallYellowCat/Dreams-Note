221593d395391f19f87eb69b6b47b3edc80f8da3
豆朋伟 <doupengwei@hikvision.com>
2019-08-09 15:39:15 +0800

    增加复原ip的操作

--- src/com/doudou/leetcode/ThreeMainTest.java
+++ src/com/doudou/leetcode/ThreeMainTest.java
@@ -340,6 +340,36 @@ public class ThreeMainTest {
     }
 
 
+    /**
+     * leetcode 93题，复原所有可能的ip地址
+     * 思想： dfs+回溯
+     * @param s
+     * @return
+     */
+    public List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        int n = s.length();
+        backtrack(0, "", 4, s, res, n);
+        return res;
+    }
+
+    private void backtrack(int i, String tmp, int flag, String s, List<String> res, int n) {
+        if (i == n && flag == 0) {
+            res.add(tmp.substring(0, tmp.length() - 1));
+            return;
+        }
+        if (flag < 0) return;
+        for (int j = i; j < i + 3; j++) {
+            if (j < n) {
+                if (i == j && s.charAt(j) == '0') {
+                    backtrack(j + 1, tmp + s.charAt(j) + ".", flag - 1, s, res, n);
+                    break;
+                }
+                if (Integer.parseInt(s.substring(i, j + 1)) <= 255)
+                    backtrack(j + 1, tmp + s.substring(i, j + 1) + ".", flag - 1, s, res, n);
+            }
+        }
+    }
 
 
 }
