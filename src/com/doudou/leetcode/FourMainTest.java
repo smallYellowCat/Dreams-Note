156ccb732dce2ac97a9f0589f1e7194302c89be9
豆豆 <dreamdeveloper@126.com>
2019-08-18 22:03:57 +0800

    增加复原IP地址，生成不同的BST树的方法

--- src/com/doudou/leetcode/FourMainTest.java
+++ src/com/doudou/leetcode/FourMainTest.java
@@ -22,7 +22,9 @@ public class FourMainTest {
         //partition(head, 2);
         //isScramble("great", "rgeat");
         //grayCode(4);
-        numDecodings("27");
+        //numDecodings("27");
+        FourMainTest fourMainTest = new FourMainTest();
+        fourMainTest.restoreIpAddresses("25525511135");
     }
 
     /**
@@ -434,5 +436,115 @@ public class FourMainTest {
         tail.next = cur;
         return head;
     }
+
+    /**
+     * 复原ip地址
+     * @param s
+     * @return
+     */
+    int n;
+    String s;
+    LinkedList<String> segments = new LinkedList<String>();
+    ArrayList<String> output = new ArrayList<String>();
+
+    public boolean valid(String segment) {
+    /*
+    Check if the current segment is valid :
+    1. less or equal to 255
+    2. the first character could be '0'
+    only if the segment is equal to '0'
+    */
+        int m = segment.length();
+        if (m > 3)
+            return false;
+        return (segment.charAt(0) != '0') ? (Integer.valueOf(segment) <= 255) : (m == 1);
+    }
+
+    public void update_output(int curr_pos) {
+    /*
+    Append the current list of segments
+    to the list of solutions
+    */
+        String segment = s.substring(curr_pos + 1, n);
+        if (valid(segment)) {
+            segments.add(segment);
+            output.add(String.join(".", segments));
+            segments.removeLast();
+        }
+    }
+
+    public void backtrack(int prev_pos, int dots) {
+    /*
+    prev_pos : the position of the previously placed dot
+    dots : number of dots to place
+    */
+        // The current dot curr_pos could be placed
+        // in a range from prev_pos + 1 to prev_pos + 4.
+        // The dot couldn't be placed
+        // after the last character in the string.
+        int max_pos = Math.min(n - 1, prev_pos + 4);
+        for (int curr_pos = prev_pos + 1; curr_pos < max_pos; curr_pos++) {
+            String segment = s.substring(prev_pos + 1, curr_pos + 1);
+            if (valid(segment)) {
+                segments.add(segment);  // place dot
+                if (dots - 1 == 0)      // if all 3 dots are placed
+                    update_output(curr_pos);  // add the solution to output
+                else
+                    backtrack(curr_pos, dots - 1);  // continue to place dots
+                segments.removeLast();  // remove the last placed dot
+            }
+        }
+    }
+
+    public List<String> restoreIpAddresses(String s) {
+        n = s.length();
+        this.s = s;
+        backtrack(-1, 3);
+        return output;
+    }
+
+
+    /**
+     * leetcode 95 不同的二叉搜索树
+     * @param start
+     * @param end
+     * @return
+     */
+    public LinkedList<TreeNode> generate_trees(int start, int end) {
+        LinkedList<TreeNode> all_trees = new LinkedList<>();
+        if (start > end) {
+            all_trees.add(null);
+            return all_trees;
+        }
+
+        // pick up a root
+        for (int i = start; i <= end; i++) {
+            // all possible left subtrees if i is choosen to be a root
+            LinkedList<TreeNode> left_trees = generate_trees(start, i - 1);
+
+            // all possible right subtrees if i is choosen to be a root
+            LinkedList<TreeNode> right_trees = generate_trees(i + 1, end);
+
+            // connect left and right trees to the root i
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
+                }
+            }
+        }
+        return all_trees;
+    }
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<>();
+        }
+        return generate_trees(1, n);
+    }
+
+
 
 }
