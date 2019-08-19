91dd4db6aa98c657cc909273a3c5fa7a40b3d9e4
豆豆 <dreamdeveloper@126.com>
2019-08-20 00:06:18 +0800

    增加计算不同的二叉搜索树

--- src/com/doudou/leetcode/FiveMainTest.java
+++ src/com/doudou/leetcode/FiveMainTest.java
@@ -81,4 +81,23 @@ public class FiveMainTest {
     }
 
 
+    /**
+     * leetcode 96 计算不同的二叉搜索树
+     * @param n
+     * @return
+     */
+    public int numTrees(int n) {
+        int[] G = new int[n + 1];
+        G[0] = 1;
+        G[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+
 }
