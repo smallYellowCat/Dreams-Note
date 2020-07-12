dd1d5713c2551410e7f74fe0a83886d6b72d0a78
豆豆 <dreamdeveloper@126.com>
2020-07-12 16:17:33 +0800

    删除无用的空行，增加六大设计原则

---
+++ Base/src/main/java/com/doudou/dp/build/FactoryPattern.java
@@ -0,0 +1,25 @@
+package com.doudou.dp.build;
+
+/**
+ * 工厂模式： 简单工厂， 抽象工厂, 工厂方法
+ * Note：在23种设计模式之中不包含简单工厂
+ * 思考：为什么使用工厂模式，工厂模式解决了什么问题。
+ */
+public class FactoryPattern {
+
+
+    public static void main(String[] args) {
+        String s = new String();
+    }
+
+    //FactoryBean
+    // 简单工厂
+    //抽象工厂
+    //工厂方法
+    
+}
+
+
+interface A {
+
+}
