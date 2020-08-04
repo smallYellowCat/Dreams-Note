976a169dbb95640892c1113c8d10f87c34a858a3
豆豆 <dreamdeveloper@126.com>
2020-08-05 00:18:23 +0800

    [ADD]增加分布式事务得开头，待补充完成，come on。

--- Base/src/main/java/com/doudou/distribute/DistributeTransaction.java
+++ Base/src/main/java/com/doudou/distribute/DistributeTransaction.java
@@ -1,4 +1,32 @@
 package com.doudou.distribute;
 
+/**
+ * 分布式事务
+ */
 public class DistributeTransaction {
+    //在说分布式事务之前首先得说下普通得事务，即本地事务
+
+    /**
+     * 本地事务
+     */
+    interface Transaction {
+        //首先四个特性描绘了事务得架构, ACID模型
+        /*
+        * 1. atomicity，原子性
+        * 2. consistency， 一致性
+        * 3. isolation， 隔离性
+        * 4. durability， 持久性
+        * */
+
+    }
+
+
+    //讲完了本地事务就讲下分布式事务
+
+    /**
+     * 分布式事务
+     */
+    interface IDistributeTransaction {
+
+    }
 }
